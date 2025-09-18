93e390a0a4035202849197bd60bdd9b3febb2c2e
David Eftink <david.eftink@pfizer.com>
2025-09-18 12:31:25 -0500

    Refactor BikeShare data loading and filtering

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -6,6 +6,12 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+MONTHS = ['january', 'february', 'march', 'april', 'may', 'june']
+
+DAYSOFWEEK = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 
+              'saturday', 'sunday']
+
+
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
@@ -16,16 +22,53 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    
+    # Get user input for city (chicago, new york city, washington)
+    is_city = 'n'
+    while is_city == 'n':
+        city_list = list(CITY_DATA.keys())
+        print("\nThe following cities can be used to filter the data.")
+        print(city_list)
+        city = input("Enter the city name: ").lower()
+        
+        if city in CITY_DATA:
+            is_city = 'y'
+            print("\nThe city '{}' has been selected.".format(city))
+        else:
+            print("\nCity not found!")
 
+    # Get user input for month (all, january, february, ... , june)
+    is_month = 'n'
+    while is_month == 'n':
+        month_list = list(MONTHS)
+        month_list.insert(0, "all")
+        print("\nThe following months can be used to filter the data.")
+        print(month_list)
+        month = input("Enter the month: ").lower()
+        
+        if month in month_list:
+            is_month = 'y'
+            print("\nThe month '{}' has been selected.".format(month))
+        else:
+            print("\nMonth not found!")
 
-    # get user input for month (all, january, february, ... , june)
-
-
-    # get user input for day of week (all, monday, tuesday, ... sunday)
-
+    # Get user input for day of week (all, monday, tuesday, ... sunday)
+    is_day = 'n'
+    while is_day == 'n':
+        day_list = list(DAYSOFWEEK)
+        day_list.insert(0, "all")
+        print("\nThe following days can be used to filter the data.")
+        print(day_list)
+        day = input("Enter the day: ").lower()
+        
+        if day in day_list:
+            is_day = 'y'
+            print("\nThe day '{}' has been selected.".format(day))
+        else:
+            print("\nDay not found!")
 
     print('-'*40)
+    
     return city, month, day
 
 
@@ -40,8 +83,31 @@ def load_data(city, month, day):
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
+    
+    # Load data file into a dataframe
+    df = pd.read_csv(CITY_DATA[city])
 
+    # Convert the Start Time column to datetime
+    df['Start Time'] = pd.to_datetime(df['Start Time'])
 
+    # Extract month, day of week, & hour from Start Time to create new columns
+    df['Month'] = df['Start Time'].dt.month
+    df['Day of Week'] = df['Start Time'].dt.weekday_name
+    df['Hour'] = df['Start Time'].dt.hour
+
+    # Filter by month if applicable
+    if month != 'all':
+        # Use the index of the months list to get the corresponding int
+        month = MONTHS.index(month) + 1
+    
+        # Filter by month to create the new dataframe
+        df = df[df['Month'] == month]
+
+    # Filter by day of week if applicable
+    if day != 'all':
+        # Filter by day of week to create the new dataframe
+        df = df[df['Day of Week'] == day.title()]
+        
     return df
 
 
@@ -147,7 +213,7 @@ def main():
         df = load_data(city, month, day)
 
         get_raw_data(df)
-
+        
         stats = input('\nWould you like to see data statistics? Enter yes or no.\n')
         if stats.lower() in ['yes', 'y']:
             time_stats(df)
